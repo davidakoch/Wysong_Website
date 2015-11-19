cbf6a258e8e14df56b00b2379079d312edf2c5b2
David A Koch <kodave1@gmail.com>
2015-11-18 22:20:31 -0600

    added slidetoggle to hamburger menu

---
+++ scripts/main.js
@@ -0,0 +1,16 @@
+$(document).on('ready', function(){
+//makes the navMenu appear when click on hamburger
+
+$('.js-menu').on('click', function(){
+	$('.main_nav').slideToggle();
+
+});
+
+
+
+
+
+
+
+
+});//end of document.on ready
