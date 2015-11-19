471a96b466d903bfb2ee1d1c7975b5f2f2f5ecdb
David A Koch <kodave1@gmail.com>
2015-11-19 00:00:31 -0600

    added 400px to main header during click function

--- scripts/main.js
+++ scripts/main.js
@@ -2,14 +2,15 @@ $(document).on('ready', function(){
 //makes the navMenu appear when click on hamburger
 
 $('.js-menu').on('click', function(){
-	$('.main_nav').slideToggle();
-	$('.main_header').animate({height:'400px'}, 500);
+	$('.main_nav').slideToggle(function(){
+		$('.main_header').animate({height:'400px'}, 100);
+	});
 });
 
 
 
 
-
+//$('.main_header').animate({height:'400px'}, 500);
 
 
 
