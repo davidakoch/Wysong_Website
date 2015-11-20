a5fad4a3ff21d1b8559748518b9bffcf4f4c3d8f
David A Koch <kodave1@gmail.com>
2015-11-19 21:51:13 -0600

    created jquery to make 700px banner and 700 menu appear

--- scripts/main.js
+++ scripts/main.js
@@ -9,10 +9,13 @@ $(document).on('ready', function(){
 
 	});
 
+	//makes the 700px banner appear when you hit close
+	$('.js_click_banner').on('click', function(){
+		$('.banner_700px').css("display","block");
+		$('.menu_700px').css("display","none");
+		$('.main_header').css("height", 200);
+	});
 
-
-
-
-
+	//makes menu and banner menu disappear when you stretch
 
 });//end of document.on ready
