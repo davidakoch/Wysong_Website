7d8878e9cc79cd7bcf9a72d45a419dfb8367b7ad
David A Koch <kodave1@gmail.com>
2015-12-07 21:40:55 -0500

    added header lengthto javascript

--- scripts/main.js
+++ scripts/main.js
@@ -9,14 +9,14 @@ $(document).on('ready', function(){
 
 	});
 
-	//makes the 700px banner appear when you hit close
+	//makes the 700px menu banner appear when you hit close
 	$('.js_click_banner').on('click', function(){
 		$('.banner_700px').css("display","block");
 		$('.menu_700px').css("display","none");
-		$('.main_header').css("height", 200);
+		$('.main_header').css("height", 225);
 	});
 
-	//makes banner 700px disappear when you stretch
+	//makes banner 700px disappear when you stretch the screen beyond 700px
 
 	$(window).on('resize', function(){
 		var w = $(window).width(); 
@@ -31,7 +31,7 @@ $(document).on('ready', function(){
 		var w = $(window).width();
 		if (w > 680 && $('.menu_700px').is(':visible')){ 
 			$('.menu_700px').removeAttr('style');
-			$('.main_header').css("height", 200);
+			$('.main_header').css("height", 225);
 		}	
 	});
 
